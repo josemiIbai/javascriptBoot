42fa8df51bc3431ed2c1916b1baea7c02d49c8b1
josemiIbai <120632830+josemiIbai@users.noreply.github.com>
2023-03-10 13:57:03 +0100

    Add files via upload

---
+++ segundo_proyecto_node/tipo_datos.js
@@ -0,0 +1,16 @@
+// Tipo de datos
+// string
+let nombre = "Josemi";
+// number
+let edad = 37;
+// boolean
+let desarrollador = true;
+// date
+let nacimiento = new Date(1985,3,04);
+// objeto
+const libro = {
+    titulo:"LA HISTORIA INTERMINABLE",
+    autor:"MICHAEL ENDE",
+    fecha: 1979,
+    url:"https://es.wikipedia.org/wiki/La_historia_interminable"
+}
