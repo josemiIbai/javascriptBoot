ac8389ecb96838f6c975d75ba01bffcf86564f1f
josemiIbai <120632830+josemiIbai@users.noreply.github.com>
2023-03-10 13:43:53 +0100

    Add files via upload

---
+++ tercer_proyecto_node/factorial_break.js
@@ -0,0 +1,9 @@
+let num = 1;
+let i = 1;
+
+while(i<=10){
+    num = num * i;
+    i++; 
+}
+
+console.log(num);
---
+++ tercer_proyecto_node/factorial_for.js
@@ -0,0 +1,5 @@
+let num=1;
+for(var i=1;i<=10;i++){
+    num = num * i;
+}
+console.log(num);
